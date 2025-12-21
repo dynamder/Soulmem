35221c4bfdf92b91a934fb9b873726eebe247f03
seeleseelesee <2448828513@qq.com>
2025-12-21 20:54:01 +0800

    情境性记忆

---
+++ src/memory/memory_note/situation_mem.rs
@@ -0,0 +1,108 @@
+use chrono::{DateTime, Utc};
+
+//一种抽象性情景记忆、一种具体性情景记忆
+#[derive(Debug, Eq, PartialEq, Ord, PartialOrd, Clone)]
+pub enum SituationType {
+    AbstractSituation(AbstractSituation),
+    SpecificSituation(SpecificSituation),
+}
+
+//抽象性情景记忆（地点、人物、事件、情境）
+#[derive(Debug, Eq, PartialEq, Ord, PartialOrd, Clone)]
+pub enum AbstractSituation {
+    Location(Location),
+    Participant(Participant),
+    Situation(Situation),
+    Event(Event),
+}
+
+//具体性情景记忆（叙述、时间、描述）
+#[derive(Debug, Eq, PartialEq, Ord, PartialOrd, Clone)]
+pub struct SpecificSituation {
+    narrative: String,
+    time_span: DateTime<Utc>,
+    context: Context,
+}
+
+impl SpecificSituation {
+    pub fn new(narrative: String, time_span: DateTime<Utc>, context: context) -> Self {
+        SpecificSituation {
+            narrative,
+            time_span,
+            context,
+        }
+    }
+    pub fn get_narrative(&self) -> &String {
+        &self.narrative
+    }
+    pub fn mut_narrative(&mut self) -> &mut String {
+        &mut self.narrative
+    }
+    pub fn get_time_span(&self) -> &DateTime<Utc> {
+        &self.time_span
+    }
+    pub fn mut_time_span(&mut self) -> &mut DateTime<Utc> {
+        &mut self.time_span
+    }
+    pub fn get_context(&self) -> &context {
+        &self.context
+    }
+    pub fn mut_context(&mut self) -> &mut context {
+        &mut self.context
+    }
+}
+
+//描述（地点、人物、情感、感官数据、情境）
+#[derive(Debug, Eq, PartialEq, Ord, PartialOrd, Clone)]
+pub struct Context {
+    location: Option<Location>,
+    participants: Vec<Participant>,
+    emotions: Vec<Emotion>,
+    sensory_data: Vec<SensoryData>,
+    situation: String,
+}
+
+impl Context {
+    pub fn new(
+        location: Option<Location>,
+        participants: Vec<participant>,
+        emotions: Vec<emotion>,
+        sensory_data: Vec<SensoryData>,
+        situation: String,
+    ) -> Self {
+        context {
+            location,
+            participants,
+            emotions,
+            sensory_data,
+            situation,
+        }
+    }
+    pub fn mut_location(&mut self) -> &mut Option<Location> {
+        &mut self.location
+    }
+    pub fn mut_participants(&mut self) -> &mut Vec<participant> {
+        &mut self.participants
+    }
+    pub fn mut_emotions(&mut self) -> &mut Vec<emotion> {
+        &mut self.emotions
+    }
+    pub fn mut_sensory_data(&mut self) -> &mut Vec<SensoryData> {
+        &mut self.sensory_data
+    }
+    pub fn mut_situation(&mut self) -> &mut String {
+        &mut self.situation
+    }
+}
+pub struct Emotion {
+    pub name: String,
+    pub intensity: f32,
+}
+pub struct Participant {
+    pub name: String,
+    pub role: String,
+}
+pub struct Location {
+    pub name: String,
+    pub coordinates: String,
+}
