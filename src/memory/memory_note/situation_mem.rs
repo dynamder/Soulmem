902a6d0ce6abfbc17943cde31dddf7d7ff8b9ebd
seeleseelesee <2448828513@qq.com>
2026-01-19 00:27:29 +0800

    情境性记忆改，坨

--- src/memory/memory_note/situation_mem.rs
+++ src/memory/memory_note/situation_mem.rs
@@ -7,6 +7,17 @@ pub enum SituationType {
     SpecificSituation(SpecificSituation),
 }
 
+impl From<AbstractSituation> for SituationType {
+    fn from(situation: AbstractSituation) -> Self {
+        SituationType::AbstractSituation(situation)
+    }
+}
+impl From<SpecificSituation> for SituationType {
+    fn from(situation: SpecificSituation) -> Self {
+        SituationType::SpecificSituation(situation)
+    }
+}
+
 //抽象性情景记忆（地点、人物、情境、事件）
 #[derive(Debug, Eq, PartialEq, Ord, PartialOrd, Clone)]
 pub enum AbstractSituation {
@@ -15,6 +26,28 @@ pub enum AbstractSituation {
     Situation(Situation),
     Event(Event),
 }
+
+impl From<Location> for AbstractSituation {
+    fn from(location: Location) -> Self {
+        AbstractSituation::Location(location)
+    }
+}
+impl From<Participant> for AbstractSituation {
+    fn from(participant: Participant) -> Self {
+        AbstractSituation::Participant(participant)
+    }
+}
+impl From<Situation> for AbstractSituation {
+    fn from(situation: Situation) -> Self {
+        AbstractSituation::Situation(situation)
+    }
+}
+impl From<Event> for AbstractSituation {
+    fn from(event: Event) -> Self {
+        AbstractSituation::Event(event)
+    }
+}
+
 
 //具体性情景记忆（叙述、时间、描述）
 #[derive(Debug, Eq, PartialEq, Ord, PartialOrd, Clone)]
@@ -35,19 +68,19 @@ impl SpecificSituation {
     pub fn get_narrative(&self) -> &String {
         &self.narrative
     }
-    pub fn mut_narrative(&mut self) -> &mut String {
+    pub fn get_mut_narrative(&mut self) -> &mut String {
         &mut self.narrative
     }
     pub fn get_time_span(&self) -> &DateTime<Utc> {
         &self.time_span
     }
-    pub fn mut_time_span(&mut self) -> &mut DateTime<Utc> {
+    pub fn get_mut_time_span(&mut self) -> &mut DateTime<Utc> {
         &mut self.time_span
     }
     pub fn get_context(&self) -> &Context {
         &self.context
     }
-    pub fn mut_context(&mut self) -> &mut Context {
+    pub fn get_mut_context(&mut self) -> &mut Context {
         &mut self.context
     }
 }
@@ -59,7 +92,7 @@ pub struct Context {
     participants: Vec<Participant>,
     emotions: Vec<Emotion>,
     sensory_data: Vec<SensoryData>,
-    situation: String,
+    situation: Situation,
 }
 
 impl Context {
@@ -68,7 +101,7 @@ impl Context {
         participants: Vec<Participant>,
         emotions: Vec<Emotion>,
         sensory_data: Vec<SensoryData>,
-        situation: String,
+        situation: Situation,
     ) -> Self {
         Context {
             location,
@@ -78,61 +111,145 @@ impl Context {
             situation,
         }
     }
-    pub fn mut_location(&mut self) -> &mut Option<Location> {
+    pub fn get_mut_location(&mut self) -> &mut Option<Location> {
         &mut self.location
     }
-    pub fn mut_participants(&mut self) -> &mut Vec<Participant> {
+    pub fn get_mut_participants(&mut self) -> &mut Vec<Participant> {
         &mut self.participants
     }
-    pub fn mut_emotions(&mut self) -> &mut Vec<Emotion> {
+    pub fn get_mut_emotions(&mut self) -> &mut Vec<Emotion> {
         &mut self.emotions
     }
-    pub fn mut_sensory_data(&mut self) -> &mut Vec<SensoryData> {
+    pub fn get_mut_sensory_data(&mut self) -> &mut Vec<SensoryData> {
         &mut self.sensory_data
     }
-    pub fn mut_situation(&mut self) -> &mut String {
+    pub fn get_mut_situation(&mut self) -> &mut Situation {
         &mut self.situation
     }
 }
 
-//事件（动作，动作强度）
+
+
+//事件（动作，动作强度）（抽象）
 #[derive(Debug, Eq, PartialEq, Ord, PartialOrd, Clone)]
 pub struct Event {
     pub action: String,
     pub action_intensity: i32,
 }
+impl TryFrom<AbstractSituation> for Event {
+    type Error = String;
 
-//情景（氛围，环境色调）
+    fn try_from(value: AbstractSituation) -> Result<Self, Self::Error> {
+        match value {
+            AbstractSituation::Event(Event{ action, action_intensity }) => Ok(Event { action, action_intensity }),
+            _ => Err("Cannot convert AbstractSituation to Event".to_string()),
+        }
+    }
+}
+
+//情景（氛围，环境色调）（抽象、描述）
 #[derive(Debug, Eq, PartialEq, Ord, PartialOrd, Clone)]
 pub struct Situation {
     pub atmosphere: String,
     pub tone: String,
 }
+impl TryFrom<AbstractSituation> for Situation {
+    type Error = String;
 
-//智能体情绪（名称，强度）
+    fn try_from(value: AbstractSituation) -> Result<Self, Self::Error> {
+        match value {
+            AbstractSituation::Situation(Situation{ atmosphere, tone }) => Ok(Situation { atmosphere, tone }),
+            _ => Err("Cannot convert AbstractSituation to Situation".to_string()),
+        }
+    }
+}
+impl From<Context> for Situation {
+    fn from(context: Context) -> Self {
+        Situation {
+            atmosphere: context.situation.atmosphere,
+            tone: context.situation.tone,
+        }
+    }
+}
+
+//智能体情绪（名称，强度）（描述）
 #[derive(Debug, Eq, PartialEq, Ord, PartialOrd, Clone)]
 pub struct Emotion {
     pub name: String,
     pub intensity: i32,
 }
+impl From<Context> for Vec<Emotion> {
+    fn from(context: Context) -> Vec<Emotion> {
+        context.emotions.into_iter().map(|emotion| Emotion {
+            name: emotion.name,
+            intensity: emotion.intensity,
+        }).collect()
+    }
+}
 
-//记忆时间主动参与者（名称，角色）
+//记忆时间主动参与者（名称，角色）(抽象、描述)
 #[derive(Debug, Eq, PartialEq, Ord, PartialOrd, Clone)]
 pub struct Participant {
     pub name: String,
     pub role: String,
 }
+impl TryFrom<AbstractSituation> for Participant {
+    type Error = String;
 
-//地点（名称，坐标）
+    fn try_from(value: AbstractSituation) -> Result<Self, Self::Error> {
+        match value {
+            AbstractSituation::Participant(Participant { name, role }) => Ok(Participant { name, role }),
+            _ => Err("Cannot convert AbstractParticipant to Participant".to_string()),
+        }
+    }
+}
+impl From<Context> for Vec<Participant> {
+    fn from(context: Context) -> Vec<Participant> {
+        context.participants.into_iter().map(|participant| Participant {
+            name: participant.name,
+            role: participant.role,
+        }).collect()
+    }
+}
+
+//地点（名称，坐标）(抽象、描述)
 #[derive(Debug, Eq, PartialEq, Ord, PartialOrd, Clone)]
 pub struct Location {
     pub name: String,
     pub coordinates: String,
 }
+impl TryFrom<AbstractSituation> for Location {
+    type Error = String;
 
-//传感数据（名称，强度）
+    fn try_from(value: AbstractSituation) -> Result<Self, Self::Error> {
+        match value {
+            AbstractSituation::Location(Location { name, coordinates }) => Ok(Location { name, coordinates }),
+            _ => Err("Cannot convert AbstractSituation to Location".to_string()),
+        }
+    }
+}
+impl TryFrom<Context> for Location {
+    type Error = String;
+
+    fn try_from(context: Context) -> Result<Self, Self::Error> {
+        match context.location {
+            Some(Location { name, coordinates }) => Ok(Location { name, coordinates }),
+            None => Err("Cannot convert Context to Location".to_string()),
+        }
+    }
+}
+
+//传感数据（名称，强度）（描述）
 #[derive(Debug, Eq, PartialEq, Ord, PartialOrd, Clone)]
 pub struct SensoryData {
     pub name: String,
     pub intensity: i32,
+}
+impl From<Context> for Vec<SensoryData> {
+    fn from(context: Context) -> Vec<SensoryData> {
+        context.sensory_data.into_iter().map(|sensory_data| SensoryData {
+            name: sensory_data.name,
+            intensity: sensory_data.intensity,
+        }).collect()
+    }
 }
