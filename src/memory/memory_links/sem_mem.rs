de69ece26d912f6dad62b7c0b0fbebf0bd2b4b82
Ron <ron@RonMacBook-Air.local>
2026-01-19 16:48:03 +0800

    modify sem_memory

--- src/memory/memory_links/sem_mem.rs
+++ src/memory/memory_links/sem_mem.rs
@@ -1,45 +1,19 @@
 use serde::{Deserialize, Serialize};
 
-//语义记忆Link
+/// 语义记忆Link
 #[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
 pub struct SemMemLink {
-    verb: String,
-    intensity: f32,
-    confidence: f32,
+    pub verb: String,
+    pub intensity: f32,
+    pub confidence: f32,
 }
 
 impl SemMemLink {
     pub fn new(verb: String, intensity: f32, confidence: f32) -> Self {
-        SemMemLink {
+        Self {
             verb,
             intensity,
             confidence,
         }
-    }
-    //谓词
-    pub fn get_verb(&self) -> &str {
-        &self.verb
-    }
-
-    pub fn set_verb(&mut self, verb: String) {
-        self.verb = verb;
-    }
-
-    //连接强度
-    pub fn get_intensity(&self) -> f32 {
-        self.intensity
-    }
-
-    pub fn set_intensity(&mut self, intensity: f32) {
-        self.intensity = intensity;
-    }
-
-    //置信度
-    pub fn get_confidence(&self) -> f32 {
-        self.confidence
-    }
-
-    pub fn set_confidence(&mut self, confidence: f32) {
-        self.confidence = confidence;
     }
 }
--- src/memory/memory_note/sem_mem.rs
+++ src/memory/memory_note/sem_mem.rs
@@ -7,48 +7,22 @@ pub enum ConceptType {
     Abstract,
 }
 
-impl ConceptType {
-    pub fn new_entity() -> Self {
-        Self::Entity
-    }
-
-    pub fn new_abstract() -> Self {
-        Self::Abstract
-    }
-}
-
 // 语义记忆节点
 #[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
 pub struct SemMemory {
-    content: String,
-    aliases: Vec<String>,
-    concept_type: ConceptType,
-    description: String,
+    pub content: String,
+    pub aliases: Vec<String>,
+    pub concept_type: ConceptType,
+    pub description: String,
 }
 
 impl SemMemory {
     pub fn new(content: String, concept_type: ConceptType, description: String) -> Self {
-        SemMemory {
+        Self {
             content,
             aliases: Vec::new(),
             concept_type,
             description,
         }
-    }
-
-    pub fn get_content(&self) -> &str {
-        &self.content
-    }
-
-    pub fn get_aliases(&self) -> &[String] {
-        &self.aliases
-    }
-
-    pub fn get_concept_type(&self) -> &ConceptType {
-        &self.concept_type
-    }
-
-    pub fn get_description(&self) -> &str {
-        &self.description
     }
 }
