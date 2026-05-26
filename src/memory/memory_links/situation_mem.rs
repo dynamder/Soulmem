d4f24a842351b66714346d34101f6bd4424b03d1
dynamder <igaster@163.com>
2026-05-26 11:13:29 +0800

    fix(memory_link): 删除情境记忆连接中的冗余字段

--- src/memory/memory_links/situation_mem.rs
+++ src/memory/memory_links/situation_mem.rs
@@ -8,27 +8,9 @@ pub enum SituationMemLink {
 }
 
 #[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
-pub struct AbstractToSpecific {
-    pub abstract_memory_id: MemoryId,
-    pub specific_memory_id: MemoryId,
-}
+pub struct AbstractToSpecific {}
 impl AbstractToSpecific {
-    pub fn new(abstract_memory_id: MemoryId, specific_memory_id: MemoryId) -> Self {
-        AbstractToSpecific {
-            abstract_memory_id,
-            specific_memory_id,
-        }
-    }
-    pub fn change_specific_memory(&mut self, memory_id: MemoryId) {
-        self.specific_memory_id = memory_id;
-    }
-    pub fn change_abstract_memory(&mut self, memory_id: MemoryId) {
-        self.abstract_memory_id = memory_id;
-    }
-    pub fn get_specific_memories(&self) -> &MemoryId {
-        &self.specific_memory_id
-    }
-    pub fn get_abstract_memory_id(&self) -> MemoryId {
-        self.abstract_memory_id
+    pub fn new() -> Self {
+        AbstractToSpecific {}
     }
 }
